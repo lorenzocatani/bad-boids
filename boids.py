515a4cf89f4d6829bbcf9783cda5daff2d5d6b6d
lorenzocatani <tedelori@libero.it>
2014-11-09 17:57:59 +0000

    class creation

--- boids.py
+++ boids.py
@@ -26,18 +26,16 @@ speed_tuning_index = 0.125
 separation1=100
 separation2=10000
 
+class boid(object):
+ def __init__(self,x,y,xv,yv):
+  self.position_x=x
+  self.position_y=y
+  self.velocity_x=xv
+  self.velocity_y=yv
 
- 
-
-#class boids (object):
- #def __init__(self,x,y,xv,yv)
-  #self.x=x
-  #self.y=y
-  #self.xv=xv
-  #self.yv=yv
   
-boids=[{"position_x":random.uniform(min_x,max) ,"position_y":random.uniform(min_y,max),
-  "velocity_x":random.uniform(min_vel_x,max_vel_x),"velocity_y":random.uniform(-vel_y,vel_y)} for i in range(50)]
+boids=[boid(random.uniform(min_x,max) ,random.uniform(min_y,max),
+  random.uniform(min_vel_x,max_vel_x),random.uniform(-vel_y,vel_y)) for i in range(50)]
 
 boids_num = len(boids)  
   
@@ -68,35 +66,35 @@ def update_boids(boids):
 	# Fly towards the middle
 	for i in range(boids_num):
 		for j in range(boids_num):
-		   boids[i]["velocity_x"] = fly_towards_middle(boids[i]["velocity_x"],boids[j]["position_x"],boids[i]["position_x"])
-		   boids[i]["velocity_y"] = fly_towards_middle(boids[i]["velocity_y"],boids[j]["position_y"],boids[i]["position_y"])
+		   boids[i].velocity_x = fly_towards_middle(boids[i].velocity_x,boids[j].position_x,boids[i].position_x)
+		   boids[i].velocity_y = fly_towards_middle(boids[i].velocity_y,boids[j].position_y,boids[i].position_y)
 	# Fly away from nearby boids
 	for i in range(boids_num):
 		for j in range(boids_num):
-		    if distance_square(boids[j]["position_x"],boids[i]["position_x"],boids[j]["position_y"],boids[i]["position_y"]) < separation1:
-			boids[i]["velocity_x"] = fly_away_from_neighbours(boids[i]["velocity_x"],boids[i]["position_x"],boids[j]["position_x"])
-		        boids[i]["velocity_y"] = fly_away_from_neighbours(boids[i]["velocity_y"],boids[i]["position_y"],boids[j]["position_y"])
+		    if distance_square(boids[j].position_x,boids[i].position_x,boids[j].position_y,boids[i].position_y) < separation1:
+			boids[i].velocity_x = fly_away_from_neighbours(boids[i].velocity_x,boids[i].position_x,boids[j].position_x)
+		        boids[i].velocity_y = fly_away_from_neighbours(boids[i].velocity_y,boids[i].position_y,boids[j].position_y)
 	# Try to match speed with nearby boids
 	for i in range(boids_num):
 		for j in range(boids_num):
-			if distance_square(boids[j]["position_x"],boids[i]["position_x"],boids[j]["position_y"],boids[i]["position_y"]) < separation2:
-			 boids[i]["velocity_x"] = match_speed_neighbours(boids[i]["velocity_x"],boids[j]["velocity_x"])
-		         boids[i]["velocity_y"] = match_speed_neighbours(boids[i]["velocity_y"],boids[j]["velocity_y"])
+			if distance_square(boids[j].position_x,boids[i].position_x,boids[j].position_y,boids[i].position_y) < separation2:
+			 boids[i].velocity_x = match_speed_neighbours(boids[i].velocity_x,boids[j].velocity_x)
+		         boids[i].velocity_y = match_speed_neighbours(boids[i].velocity_y,boids[j].velocity_y)
 	# Move according to velocities
 	for i in range(len(boids)):
-		boids[i]["position_x"] =boids[i]["position_x"] + boids[i]["velocity_x"]
-		boids[i]["position_y"] =boids[i]["position_y"] + boids[i]["velocity_y"]
+		boids[i].position_x =boids[i].position_x + boids[i].velocity_x
+		boids[i].position_y =boids[i].position_y + boids[i].velocity_y
 
-positions_x = [boids[i]["position_x"] for i in range(50)]
-positions_y = [boids[i]["position_y"] for i in range(50)]
+positions_x = [boids[i].position_x for i in range(50)]
+positions_y = [boids[i].position_y for i in range(50)]
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(positions_x,positions_y)
 
 def animate(frame):
-   positions_x = [boids[i]["position_x"] for i in range(50)]
-   positions_y = [boids[i]["position_y"] for i in range(50)]
+   positions_x = [boids[i].position_x for i in range(50)]
+   positions_y = [boids[i].position_y for i in range(50)]
    update_boids(boids)
    scatter.set_offsets(zip(positions_x,positions_y))
 
@@ -106,4 +104,3 @@ anim = animation.FuncAnimation(figure, animate,
 
 if __name__ == "__main__":
     plt.show()
-
