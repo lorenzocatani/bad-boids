a89abfd1cfdf9d92a643928d92fb86fd08324983
lorenzocatani <tedelori@libero.it>
2014-11-10 14:40:06 +0000

    test workss

--- test_boids.py
+++ test_boids.py
@@ -1,4 +1,4 @@
-from boids import update_boids
+from boids import update_boids, boid
 from nose.tools import assert_almost_equal
 import os
 import yaml
@@ -12,11 +12,19 @@ def test_bad_boids_regression():
 	
     boid_data_aft=regression_data["after"]
     new_boid_data_aft =[boid(boid_data_aft[0][i],boid_data_aft[1][i],boid_data_aft[2][i],boid_data_aft[3][i]) for i in range(50)]
-    update_boids(new_boid_data_aft)
     
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
+    for i in range(50):
+            assert_almost_equal(new_boid_data_aft[i].position_x,new_boid_data_bef[i].position_x,delta=0.01)
+	    assert_almost_equal(new_boid_data_aft[i].position_y,new_boid_data_bef[i].position_y,delta=0.01)
+	    assert_almost_equal(new_boid_data_aft[i].velocity_x,new_boid_data_bef[i].velocity_x,delta=0.01)
+	    assert_almost_equal(new_boid_data_aft[i].velocity_y,new_boid_data_bef[i].velocity_y,delta=0.01)
+	        
+	  
+	  
+	  #for j in range(4):
+	  #for after,before in zip(new_boid_data_aft[i][0],new_boid_data_bef[i][0]):
+             #for after_value,before_value in zip(after,before): 
+               # assert_almost_equal(after_value,before_value,delta=0.01)
 	
 
 	
