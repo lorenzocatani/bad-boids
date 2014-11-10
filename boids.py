d46ab5af62cd66c798caf6ef410b172f062e6564
lorenzocatani <tedelori@libero.it>
2014-11-10 16:39:59 +0000

    creation config.yaml

--- boids.py
+++ boids.py
@@ -12,22 +12,10 @@ from matplotlib import animation
 import random
 
 from numpy import array
-# Deliberately terrible code for teaching purposes
 
-#c=yaml.load(open("config.yaml"))
+c=yaml.load(open("config.yaml"))
 
-min_x = -450   
-min_y = 300 
-max_x = 50
-max_y=600
-min_vel_x = 0    
-max_vel_x = 10
-vel_y = 20 
-attraction_index = 0.01  
-speed_tuning_index = 0.125
-separation1=100
-separation2=10000
-boids_num = 50
+
 
 class boid(object):
  def __init__(self,x,y,xv,yv):
@@ -42,11 +30,11 @@ class boid(object):
  
  def fly_towards_middle_x(self,other):
   separation = other.position_x - self.position_x
-  self.velocity_x = self.velocity_x + (separation)*attraction_index/boids_num
+  self.velocity_x = self.velocity_x + (separation)*c['attraction_index']/c['boids_num']
   return self.velocity_x
  def fly_towards_middle_y(self,other):
   separation = other.position_y - self.position_y
-  self.velocity_y = self.velocity_y + (separation)*attraction_index/boids_num
+  self.velocity_y = self.velocity_y + (separation)*c['attraction_index']/c['boids_num']
   return self.velocity_y
  
  def fly_away_from_neighbours_x(self,other):
@@ -60,21 +48,20 @@ class boid(object):
   
  def match_speed_neighbours_x(self,other):
   separation = other.velocity_x-self.velocity_x
-  self.velocity_x = self.velocity_x +(separation)*speed_tuning_index/boids_num
+  self.velocity_x = self.velocity_x +(separation)*c['speed_tuning_index']/c['boids_num']
   return self.velocity_x 
  def match_speed_neighbours_y(self,other):
   separation = other.velocity_y-self.velocity_y
-  self.velocity_y = self.velocity_y +(separation)*speed_tuning_index/boids_num
+  self.velocity_y = self.velocity_y +(separation)*c['speed_tuning_index']/c['boids_num']
   return self.velocity_y 
   
   
  
-boids=[boid(random.uniform(min_x,max_x) ,random.uniform(min_y,max_y),
-  random.uniform(min_vel_x,max_vel_x),random.uniform(-vel_y,vel_y)) for i in range(boids_num)]
+boids=[boid(random.uniform(c['min_x'],c['max_x']) ,random.uniform(c['min_y'],c['max_y']),
+  random.uniform(c['min_vel_x'],c['max_vel_x']),random.uniform(c['min_vel_y'],c['max_vel_y'])) for i in range(c['boids_num'])]
 
  
  #replace global variable with function argument?
- #togliere i commenti??
  #gli ultimi due sulle classi
  #fai test
  
@@ -90,14 +77,14 @@ def update_boids(boids):
 	# Fly away from nearby boids
 	for boid1 in boids:
 	    for boid2 in boids:
-		    if  boid2.distance_square(boid1) < separation1:
+		    if  boid2.distance_square(boid1) < c['separation1']:
 		     boid1.fly_away_from_neighbours_x(boid2)
 	             boid1.fly_away_from_neighbours_y(boid2)
 				 
 	# Try to match speed with nearby boids
 	for boid1 in boids:
 	    for boid2 in boids:
-			if boid2.distance_square(boid1) < separation2:
+			if boid2.distance_square(boid1) < c['separation2']:
 			 boid1.match_speed_neighbours_x(boid2)
 	                 boid1.match_speed_neighbours_y(boid2)
 					 
@@ -106,6 +93,8 @@ def update_boids(boids):
 		boid.position_x = boid.position_x + boid.velocity_x
 		boid.position_y = boid.position_y + boid.velocity_y
 
+		
+		
 positions_x = [boids[i].position_x for i in range(50)]
 positions_y = [boids[i].position_y for i in range(50)]
 
